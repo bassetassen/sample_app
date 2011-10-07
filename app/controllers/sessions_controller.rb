1c0ae9fc1542f1ea1b3e7bde1a3e032056d6418c
Sebastian Holager <bassetassen@hotmail.com>
2011-10-07 05:28:58 +0200

    Done with user edit/update. index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       render :new
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
@@ -20,5 +20,4 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
-
 end
